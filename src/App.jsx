15b4007897d76d50444a341faddef1c38bb76b3a
SHANTO <rabbymd55555@gmail.com>
2025-08-16 19:24:15 +0600

    added tic tac toe games

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,17 @@
-function Square({value}) {
+import { useState } from "react";
+
+function Square() {
+  const [value, setValue] = useState(null);
+
+  function handleClick() {
+    setValue("X");
+  }
+
   return (
-    <button className="bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-lg">
+    <button
+      className="bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-lg"
+      onClick={handleClick}
+    >
       {value}
     </button>
   );
@@ -9,22 +20,22 @@ function Square({value}) {
 export default function Board() {
   return (
     <>
-      <div>
-        <Square value='1'></Square>
-        <Square value='2'></Square>
-        <Square value='3'></Square>
+      <div className="flex">
+        <Square></Square>
+        <Square></Square>
+        <Square></Square>
       </div>
 
-      <div>
-        <Square value='4'></Square>
-        <Square value='5'></Square>
-        <Square value='6'></Square>
+      <div className="flex">
+        <Square></Square>
+        <Square></Square>
+        <Square></Square>
       </div>
 
-      <div>
-        <Square value='7'></Square>
-        <Square value='8'></Square>
-        <Square value='9'></Square>
+      <div className="flex">
+        <Square></Square>
+        <Square></Square>
+        <Square></Square>
       </div>
     </>
   );
