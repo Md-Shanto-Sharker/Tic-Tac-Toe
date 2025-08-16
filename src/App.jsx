9625601c00a971b893f725731ad965cd25ae77b2
SHANTO <rabbymd55555@gmail.com>
2025-08-16 22:52:12 +0600

    added tic tac toe games

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,12 @@
 import { useState } from "react";
 
-function Square() {
-  const [value, setValue] = useState(null);
-
-  function handleClick() {
-    setValue("X");
-  }
+function Square({ value ,onSquareClick}) {
+  
 
   return (
     <button
       className="bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-lg"
-      onClick={handleClick}
+      onClick={onSquareClick}
     >
       {value}
     </button>
@@ -18,24 +14,30 @@ function Square() {
 }
 
 export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+    function handleClick(){
+      console.log('clicked by handleClicked');
+     
+  }
   return (
     <>
       <div className="flex">
-        <Square></Square>
-        <Square></Square>
-        <Square></Square>
+        <Square value={squares[0]} onSquareClick={handleClick}></Square>
+        <Square value={squares[1]}></Square>
+        <Square value={squares[2]}></Square>
       </div>
 
       <div className="flex">
-        <Square></Square>
-        <Square></Square>
-        <Square></Square>
+        <Square value={squares[3]}></Square>
+        <Square value={squares[4]}></Square>
+        <Square value={squares[5]}> </Square>
       </div>
 
       <div className="flex">
-        <Square></Square>
-        <Square></Square>
-        <Square></Square>
+        <Square value={squares[6]}></Square>
+        <Square value={squares[7]}></Square>
+        <Square value={squares[8]}></Square>
       </div>
     </>
   );
